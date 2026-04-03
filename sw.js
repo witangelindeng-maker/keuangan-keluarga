97261bdafc723cdf50b26ddfed2dd67ca1b8ed12
witangelindeng-maker <witangelindeng@gmail.com>
2026-04-04 00:26:10 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open("pajak-app").then(function(cache) {
+    caches.open("keuangan-keluarga").then(function(cache) {
       return cache.addAll([
         "index.html",
         "manifest.json",
